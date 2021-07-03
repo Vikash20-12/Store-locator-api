c767fabf929dd6f9a6e49345b38acab46865bfdb
Vikash20-12 <85476256+Vikash20-12@users.noreply.github.com>
2021-07-03 16:02:26 +0530

    location fetch done well

--- controllers/stores.js
+++ controllers/stores.js
@@ -1,7 +1,46 @@
+const Store = require('../models/Store');
+
 // @desc GET ALL STORES
 // @route GET api/v1/stores
 // @acess Public
 
-exports.getStores = (req, res, next)=>{
-    res.send('hello from getStores');
-}
+exports.getStores = async (req, res, next)=>{
+    try {
+        const stores = await Store.find();
+
+        return res.status(200).json({
+            success: true,
+            count: stores.length,
+            data: stores
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({error: 'Server error'});
+    }
+};
+
+
+
+// @desc CREATE A STORE
+// @route POST api/v1/stores
+// @acess Public
+
+exports.addStore = async (req, res, next)=>{
+    try {
+        const store = await Store.create(req.body);
+
+        return res.status(200).json({
+            sucess: true,
+            data: store
+        });
+    } catch (err) {
+        console.error(err);
+        if(err.code === 11000) {
+            return res.status(400).json({
+                error: 'This store already exists'
+            });
+        }
+        res.status(500).json({error: 'Server error'});
+    }
+};
+
--- routes/stores.js
+++ routes/stores.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const {getStores} = require('../controllers/stores');
+const {getStores, addStore} = require('../controllers/stores');
 
 const router = express.Router();
 
 //ROUTES
-router.route('/').get(getStores);
+router
+    .route('/')
+    .get(getStores)
+    .post(addStore);
 
 module.exports = router;
