0dd59fbdccb2e20679373beecf8839f60e4db7fa
Vikash20-12 <85476256+Vikash20-12@users.noreply.github.com>
2021-07-03 21:34:37 +0530

    stores located

--- public/js/map.js
+++ public/js/map.js
@@ -6,5 +6,58 @@ zoom: 9,
 center: [73.8567, 18.5204]
 });
 
-function loadMap() {
-}
+//FETCH STORES FROM API
+
+async function getStores() {
+    const res = await fetch('/api/v1/stores');
+    const data = await res.json();
+  
+    const stores = data.data.map(store => {
+      return {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [
+            store.location.coordinates[0],
+            store.location.coordinates[1]
+          ]
+        },
+        properties: {
+          storeId: store.storeId,
+          icon: 'shop'
+        }
+      };
+    });
+  
+    loadMap(stores);
+  }
+
+  
+
+//LOAD MAP WITH STORES
+
+function loadMap(stores) {
+    map.on('load', function() {
+      map.addLayer({
+        id: 'points',
+        type: 'symbol',
+        source: {
+          type: 'geojson',
+          data: {
+            type: 'FeatureCollection',
+            features: stores
+          }
+        },
+        layout: {
+          'icon-image': '{icon}-15',
+          'icon-size': 1.5,
+          'text-field': '{storeId}',
+          'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+          'text-offset': [0, 0.9],
+          'text-anchor': 'top'
+        }
+      });
+    });
+  }
+
+  getStores();
