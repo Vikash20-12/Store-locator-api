2215c45c3ea8c0fafee3bb6771aeefe5b8c3a262
Vikash20-12 <85476256+Vikash20-12@users.noreply.github.com>
2021-07-03 16:40:23 +0530

    frontend

---
+++ public/js/map.js
@@ -0,0 +1,10 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoidmlrYXNoMjAtMTIiLCJhIjoiY2txbm4xaTd5MGI2NjJ2cW5qNmRmem9uNCJ9.G1L7Si8zDtmtmLv5qCobsg';
+const map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+zoom: 9,
+center: [73.8567, 18.5204]
+});
+
+function loadMap() {
+}
--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.use(express.json());
 //ENABLE CORS
 app.use(cors());
 
+//SET STATIC FOLDER
+app.use(express.static(path.join(__dirname,'public')));
+
 //ROUTES
 app.use('/api/v1/stores', require('./routes/stores'));
 
